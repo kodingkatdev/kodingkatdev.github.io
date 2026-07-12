1e8e1f3e9734e37a93a063fbfb3e40c4caa598a6
kodingkat <kodingkatdev@gmail.com>
2026-07-11 22:22:52 -0400

    Deploying

--- src/js/scroll.js
+++
@@ -1,8 +0,0 @@
-document.documentElement.dataset.scroll = window.scrollY;
-document.addEventListener('scroll', () => {
-    if(window.scrollY>25){
-    document.documentElement.dataset.scroll = window.scrollY;
-    }else{
-    document.documentElement.dataset.scroll = 0;
-    }
-});
